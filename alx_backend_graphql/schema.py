b64ad08d8ae0b8027b93d5769703d0c5c0960806
Acacore <mensart81@outlook.com>
2025-10-17 20:59:35 +0000

    Query class created

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -2,3 +2,11 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from crm.models import *
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
+    
+schema = graphene.schema(query=Query)
