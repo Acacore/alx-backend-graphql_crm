61e55f2e9515970fdc45209d7d0cc01eaf877960
Acacore <mensart81@outllook.com>
2025-10-29 10:35:53 +0000

    seetings.py added to crm app

--- crm/settings.py
+++ crm/settings.py
@@ -1,3 +1,20 @@
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crm',
+    'allauth',
+    'allauth.account',
+    'django_filters',
+    'graphene_django',
+    # 'phonenumber_field'
+    'django_crontab',
+]
+
 CRONJOBS = [
     ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
 ]
